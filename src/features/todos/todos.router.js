373fb45cea7a6af096b70e4180374009786c73c4
AdiDav000 <adidavar000@gmail.com>
2024-04-03 08:29:48 +0530

    corrected

--- src/features/todos/todos.router.js
+++ src/features/todos/todos.router.js
@@ -1,18 +1,18 @@
-import { router } from "../../../server.js";
-import { list } from "../../services/data.js";
+import { router } from '../../../server.js';
+import { list } from '../../services/data.js';
 
-router.get("/list", (req, res) => {
-  res.render("../views/main.ejs", { list: list });
+router.get('/list', (req, res) => {
+  res.render('../views/main.ejs', { list });
 });
-router.post("/list/new", (req, res) => {
+router.post('/list/new', (req, res) => {
   if (req.body.todo_text.length > 0) {
     list.push({ active: true, text: req.body.todo_text });
   }
-  res.redirect("/list");
+  res.redirect('/list');
 });
 
-router.post("/list/:index", async (req, res) => {
+router.post('/list/:index', async (req, res) => {
   list[req.params.index].active = false;
-  res.redirect("/list");
+  res.redirect('/list');
 });
 export default router;
